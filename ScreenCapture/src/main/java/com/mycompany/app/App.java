e91bcd21f53d5248c1ff174976decc9d00ecc052
gurchet <sgurchet20@gmail.com>
2017-03-31 16:18:40 +0530

    Add files via upload

---
+++ ScreenCapture/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import java.awt.AWTException;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) throws IOException, AWTException {
+		new SingleButtonapp();
+//		SingleButtonapp obj_app = new SingleButtonapp();
+
+	}
+}
